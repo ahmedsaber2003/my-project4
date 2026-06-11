601955d3de78ec1b83a5a229ddbfa8348ea43b6d
ahmedsaber2003 <91927507+ahmedsaber2003@users.noreply.github.com>
2026-06-11 22:19:32 +0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <script>
-const COMPILER_API = "https://studied-score-paint-harvey.trycloudflare.com/api/run";
+const COMPILER_API = "https://southampton-trusted-inns-cruz.trycloudflare.com";
 alert(COMPILER_API);
 async function checkTask(taskId, textareaId, resultId) {
     const code = document.getElementById(textareaId).value.trim();
